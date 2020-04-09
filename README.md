0fe7930c80737f5bb65105c248a3c3fc1c7d7d90
Piotr St <no-reply1@tibiaauto.net>
2020-04-09 09:50:44 +0100

    Added topCoord, changed README

--- README.md
+++ README.md
@@ -2,11 +2,13 @@
 This is a Spaceflight Simulator BP editor in browser or (planned) for desktop
 # Todo
  - [ ] fix the resources
- - [ ] get BP generating working
+ - [x] get BP generating working
  - [ ] package as desktop app
  - [ ] package for android?
 # Credits
  - Code: Peter Stelmaszek (@pitust on github, u/pitust)
  - Resource hunting: (@FourCinnamon0 on github, u/FourCinnamon0)
+# Contributing
+You can send a PR.
 # License
 In this program I used resources from the game Spaceflight Simulator. These files are (c) Stefo Mai Morojna. The software code is (c) Piotr Stelamszek and distributed with MIT license.
--- index.js
+++ index.js
@@ -5,27 +5,32 @@ let parts = {
         'Small Aerodynamic Fueslage': {
             bpName: 'Side Cone',
             file: 'Aerodynamics/Aerodynamic_fuselage_1.png',
-            size: [2, 6]
+            size: [2, 6],
+            tl: false
         },
         'Big Aerodynamic Fueslage': {
             bpName: 'Side Cone Big',
             file: 'Aerodynamics/Aerodynamic_fuselage_2.png',
-            size: [3, 8]
+            size: [3, 8],
+            tl: false
         },
         'Fairing Cone': {
             bpName: 'Top Fairing',
             file: 'Aerodynamics/Fairing_1.png',
-            size: [4, 3]
+            size: [4, 3],
+            tl: false
         },
         'Fairing': {
             bpName: '4w Fairing',
             file: 'Aerodynamics/Fairing_2.png',
-            size: [4, 3]
+            size: [4, 3],
+            tl: false
         },
         'Nose Cone': {
             bpName: 'Cone',
             file: 'Aerodynamics/Nose_cone_1.png',
-            size: [4, 5]
+            size: [4, 5],
+            tl: false
         },
         'Side Cone': {
             bpName: 'SideSide',
@@ -35,41 +40,48 @@ let parts = {
         'Big Cone': {
             bpName: 'Big Side',
             file: 'Aerodynamics/Nose_cone_3.png',
-            size: [6, 7]
+            size: [6, 7],
+            tl: false
         }
     },
     'Batteries': {
         'Small Battery': {
             bpName: 'Battery 2x1',
             file: 'Batteries/Battery_2w.png',
-            size: [2, 1]
+            size: [2, 1],
+            tl: false
         },
         'Medium Battery': {
             bpName: 'Battery 4x1',
             file: 'Batteries/Battery_4w.png',
-            size: [4, 1]
+            size: [4, 1],
+            tl: false
         },
         'Large Battery': {
             bpName: 'Battery 6x1',
             file: 'Batteries/Battery_6w.png',
-            size: [6, 1]
+            size: [6, 1],
+            tl: false
         },
     },
     'Docking Ports': {
         'Small Docking Port': {
             bpName: 'Port 2x1',
             file: 'Docking ports/2w.png',
-            size: [2, 1]
+            size: [2, 1],
+            tl: true
         },
         'Medium Docking Port': {
             bpName: 'Port 4x1',
             file: 'Docking ports/4w.png',
-            size: [4, 1]
+            size: [4, 1],
+            tl: true
         },
         'Large Docking Port': {
             bpName: 'Port 6x1',
             file: 'Docking ports/6w.png',
-            size: [6, 1]
+            size: [6, 1],
+            tl: true
         },
     }
 }
@@ -93,7 +105,7 @@ function createBP() {
                 partName: e.type,
                 position: {
                     x: e.getPos()[0] / -2,
-                    y: e.getPos()[1] / -2
+                    y: e.getPos()[1] / -2 + (e.hy / -2)
                 },
                 orientation: {
                     "x": 1,
@@ -122,9 +134,10 @@ function summonPart(x) {
         type: p.bpName,
         getPos() {
             return [objx, objy];
-        }
+        },
+        hy: p.tl ? 0 : p.size[1]
     })
-    a.onmousedown = function(ev) {
+    a.onmousedown = function (ev) {
         objOffx = ev.pageX - (objx * 20) - 3;
         objOffy = ev.pageY - (objy * 20) - 3;
         d = !0;
@@ -134,19 +147,20 @@ function summonPart(x) {
         pe = {
             obj: a,
             rm() {
-            a.remove();
-            sic = false;
-        }};
+                a.remove();
+                sic = false;
+            }
+        };
         a.style.border = '3px solid lime';
     }
-    a.onmouseup = function() {
+    a.onmouseup = function () {
         objx = Math.round(objx);
         objy = Math.round(objy);
         a.style.left = ((objx * 20) - 3) + 'px';
         a.style.top = ((objy * 20) - 3) + 'px';
         d = !1;
     }
-    a.onmousemove = function(ev) {
+    a.onmousemove = function (ev) {
         if (!d) return;
         let realx = ev.pageX - objOffx;
         let realy = ev.pageY - objOffy;
@@ -157,7 +171,7 @@ function summonPart(x) {
         objx = realx;
         objy = realy;
     }
-    a.ondragstart = function(ev) {
+    a.ondragstart = function (ev) {
         ev.preventDefault();
         return false;
     }
@@ -170,11 +184,11 @@ function plus() {
         inputOptions: swtbl,
         inputPlaceholder: 'required',
         showCancelButton: true
-      }).then(function (result) {
+    }).then(function (result) {
         if (result.value) {
             summonPart(result.value);
         }
-      });
+    });
 }
 let o = document.querySelector('#options');
 let pe = null;
@@ -184,7 +198,7 @@ let actions = [
 for (let che of o.children) {
     che.onclick = actions.shift();
 }
-document.body.onkeydown = function(ev) {
+document.body.onkeydown = function (ev) {
     if (ev.code == 'Backspace') {
         pe.rm();
     }
